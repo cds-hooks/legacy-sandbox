12a63a4f0b43d5a8abc822a270ceed3da78269f6
Zach Plata <zach.plata@cerner.com>
2018-02-15 13:51:25 -0600

    Add patient in context calls and store relevant resource

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import smartLaunchPromise from '../../retrieve-data-helpers/smart-launch';
 import retrieveFhirMetadata from '../../retrieve-data-helpers/fhir-metadata-retrieval';
+import retrievePatient from '../../retrieve-data-helpers/patient-retrieval';
 
 class MainView extends React.Component {
   constructor(props) {
@@ -20,14 +21,18 @@ class MainView extends React.Component {
    */
   componentDidMount() {
     smartLaunchPromise().then(() => {
-      // TODO: Call patient in context
+      return retrievePatient();
     }, () => {
       // TODO: Display an error banner indicating smart launch failed, and it will launch openly
       return retrieveFhirMetadata().then(() => {
-        // TODO: Call patient in context
+        return retrievePatient();
       }, () => {
         // TODO: Manually enter a FHIR server (modal) if default FHIR server fails to load
       });
+    }).then(() => {
+      // TODO: Call CDS Services Discovery endpoints
+    }, () => {
+      // TODO: Manually enter a patient in context if Patient resource fails to return
     });
   }
 
